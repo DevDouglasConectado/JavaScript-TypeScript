61772ada19bf798ab0d2f13787f89b37e0b9492d
Douglas <douglasrodriguesconectadoifmg@gmail.com>
2023-03-01 21:42:04 -0300

    letVAR

---
+++ let var/index.js
@@ -0,0 +1,49 @@
+const verdadeira = true;
+var sobrenome;
+/*
+// let tem escopo de bloco {...bloco}
+// var só tem escopo de função
+
+let nome = 'Luiz';
+var nome2 = 'Luiz'
+
+if(verdadeira){
+    let nome = 'otavio1';
+    var nome2 = 'otavio1'
+    console.log(nome, nome2)
+  if(verdadeira){
+    let nome = 'otavio2';
+    var nome2 = 'otavio2'
+    console.log(nome, nome2)
+  }
+
+}
+console.log(nome, nome2)
+// vai imprimir: Luiz otavio2 
+
+var sobrenome = 'miranda';
+let sobrenome2 = 'mirandão'
+function falaOi(){
+    var sobrenome = 'mi';
+    let sobrenome2 = 'mi'
+
+    console.log(sobrenome,sobrenome2);
+}
+console.log(sobrenome);
+console.log(sobrenome2);
+ falaOi();*/
+
+ console.log(sobrenome);
+ sobrenome = 'Miranda';
+ //agontece o rostim
+ //undefined
+
+ console.log(sobrenome2);
+ let sobrenome2
+
+ /**ReferenceError: Cannot access 'sobrenome2' ............ */
+
+
+
+
+
