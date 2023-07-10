8cf6c3cee1090162877286c81749370cdac8b047
Douglas <douglasrodriguesconectadoifmg@gmail.com>
2023-07-09 23:14:25 -0300

    execicios praticados

--- OrigameJS/03-dom-para-iniciantes/0304-foreach-e-arrow-function/script.js
+++ OrigameJS/03-dom-para-iniciantes/0304-foreach-e-arrow-function/script.js
@@ -1,15 +1,17 @@
 // Mostre no console cada parágrado do site
+console.log("// Mostre no console cada parágrado do site");
 const paragrafos = document.querySelectorAll('p');
 console.log(paragrafos);
 
 paragrafos.forEach((item) => {
   console.log(item);
 });
-
+console.log("// Mostre o texto dos parágrafos no console");
 // Mostre o texto dos parágrafos no console
 paragrafos.forEach((item) => {
   console.log(item.innerText);
 });
+console.log("// Como corrigir os erros abaixo:");
 
 // Como corrigir os erros abaixo:
 const imgs = document.querySelectorAll('img');
@@ -26,3 +28,7 @@ imgs.forEach(() => {
 imgs.forEach(() => i++);
 
 console.log(i);
+
+console.log("************************************");
+
+
