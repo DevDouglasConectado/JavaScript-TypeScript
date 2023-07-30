7e1597079cdd25a3bbd1ebac026fd9b99e209e9f
Douglas <douglasrodriguesconectadoifmg@gmail.com>
2023-07-30 11:08:15 -0300

    execicios praticados

--- OrigameJS/03-dom-para-iniciantes/0303-selecao-de-elementos/script-aula.js
+++ OrigameJS/03-dom-para-iniciantes/0303-selecao-de-elementos/script-aula.js
@@ -12,13 +12,15 @@ console.log(primeiraLi);
 const primeiraUl = document.querySelector('ul');
 console.log(primeiraUl)
 
-/*const linkInterno = document.querySelector('[href^="#"]');
+const linkInterno = document.querySelector('[href^="#"]');
 console.log(linkInterno);
 
 const animaisImg = document.querySelectorAll('.animais img');
 console.log(animaisImg[0]);
 
 const gridSectionHTML = document.getElementsByClassName('grid-section');
+
+
 const gridSectionNode = document.querySelectorAll('.grid-section');
 
 primeiraUl.classList.add('grid-section');
@@ -34,4 +36,4 @@ const arrayGrid = Array.from(gridSectionHTML);
 
 arrayGrid.forEach(function(item) {
   console.log(item);
-})*/
+})
