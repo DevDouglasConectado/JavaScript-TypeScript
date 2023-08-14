b90a0b5b03b4bd40fa02e0789ac1b2d0444270ac
Douglas <douglasrodriguesconectadoifmg@gmail.com>
2023-08-13 23:33:08 -0300

    execicios praticados

---
+++ OrigameJS/03-dom-para-iniciantes/0308-traversing-e-manipulacao/ElementVSnode.js
@@ -0,0 +1,13 @@
+const lista = document.querySelector('.animais-lista');
+
+console.log("elementos acima");
+console.log(lista.previousElementSibling);
+
+console.log("node acima");
+console.log(lista.previousSibling);
+
+console.log("primeiro node child ");
+console.log(lista.firstChild);
+
+console.log("todos os node child");
+console.log(lista.childNodes);
